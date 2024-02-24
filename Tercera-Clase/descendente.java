a28c340457fce89507ab077343a68ffebb7e4150
ElProfeGus <161101092+ElProfeGus@users.noreply.github.com>
2024-02-24 00:00:57 -0500

    Add files via upload

---
+++ Tercera-Clase/descendente.java
@@ -0,0 +1,11 @@
+package Repositorio;
+
+public class descendente {
+    public static void main(String[] args) {
+        int num = 10;
+        
+        do{
+            System.out.println(num);
+        } while (num >= 1);
+    }
+}
---
+++ Tercera-Clase/menuopciones.java
@@ -0,0 +1,24 @@
+package Repositorio;
+
+public class menuopciones {
+    public static void main(String[] args) {
+        
+        // Crear un arrar de tamaño 3
+        int[] array = new int[3];
+        
+        // Asignar valores a los elementos del array
+        array[0] = 999110912;
+        array[1] = 998558321;
+        array[2] = 941623823;
+        
+        // Imprimir los elemntos del array
+        System.out.println("Elemento 1: " + array[0] + "\n" + 
+                " Elemento 2: " + array[1] + "\n" + 
+                " Elemento 3: " + array[2]);
+        
+        
+        
+        
+    }
+    
+}
